12b505a4d6ed20949ad66270fc7be973a00c25c0
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-11-28 15:09:21 -0500

    SEARCH PARAM

--- router-tutorial/src/routes/invoices.jsx
+++ router-tutorial/src/routes/invoices.jsx
@@ -1,28 +1,68 @@
-import { Link,  Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../data";
-
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <div style={{ display: "flex" }}>
       <nav
         style={{
           borderRight: "solid 1px",
-          padding: "1rem"
+          padding: "1rem",
         }}
       >
-        {invoices.map(invoice => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={event => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter(invoice => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "",
+              };
+            }}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />
     </div>
   );
 }
+
+//- active links Link vs NavLink https://reactrouter.com/docs/en/v6/getting-started/tutorial#active-links
+//It's very common, especially in navigation lists, to display the link as the active link the user is looking at. 
+//Let's add this treatment to our invoices list by swapping out Link for NavLink.
+
+
+/*
+normal string
+<NavLink className="red" />
+
+function
+<NavLink className={({ isActive }) => isActive ? "red" : "blue"} />
+
+-Search params https://reactrouter.com/docs/en/v6/getting-started/tutorial#search-params
+*/
