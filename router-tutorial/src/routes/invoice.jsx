7998c63a2b7f9fbd0dae6c4618b94f8e1cbe5f91
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-11-28 12:13:28 -0500

    params

--- router-tutorial/src/routes/invoice.jsx
+++ router-tutorial/src/routes/invoice.jsx
@@ -1,6 +1,17 @@
 import { useParams } from "react-router-dom";
+import { getInvoice } from "../data";
 
 export default function Invoice() {
   let params = useParams();
-  return <h2>Invoice: {params.invoiceId}</h2>;
+  let invoice = getInvoice(parseInt(params.invoiceId, 10));
+
+  return (
+    <main style={{ padding: "1rem" }}>
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>
+        {invoice.name}: {invoice.number}
+      </p>
+      <p>Due Date: {invoice.due}</p>
+    </main>
+  );
 }
